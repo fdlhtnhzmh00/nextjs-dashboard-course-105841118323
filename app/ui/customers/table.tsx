e0fb9e731d3746f42db78c6d69048494dd2b8278
fdlhtnhzmh00 <105841118323@student.unismuh.ac.id>
2025-08-11 03:46:25 +0800

    Update project files

--- app/ui/customers/table.tsx
+++ app/ui/customers/table.tsx
@@ -1,8 +1,7 @@
 import Image from 'next/image';
 import { fetchFilteredCustomers } from '@/app/lib/data';
 import { formatCurrency } from '@/app/lib/utils';
-import { StaticImport } from 'next/dist/shared/lib/get-img-props';
-import { Key, ReactElement, JSXElementConstructor, ReactNode, ReactPortal } from 'react';
+import { CustomersTableType } from '@/app/lib/definitions';
 
 export default async function CustomersTable({ query }: { query: string }) {
   const customers = await fetchFilteredCustomers(query);
@@ -12,6 +11,54 @@ export default async function CustomersTable({ query }: { query: string }) {
       <div className="overflow-x-auto">
         <div className="inline-block min-w-full align-middle">
           <div className="overflow-hidden rounded-md bg-gray-50 p-2 md:pt-0">
+            {/* ================================== */}
+            {/* == BLOK KODE UNTUK MOBILE VIEW == */}
+            {/* ================================== */}
+            <div className="md:hidden">
+              {customers?.map((customer: CustomersTableType) => (
+                <div
+                  key={customer.id}
+                  className="mb-2 w-full rounded-md bg-white p-4"
+                >
+                  <div className="flex items-center justify-between border-b pb-4">
+                    <div>
+                      <div className="mb-2 flex items-center">
+                        <div className="flex items-center gap-3">
+                          <Image
+                            src={customer.image_url}
+                            className="rounded-full"
+                            alt={`${customer.name}'s profile picture`}
+                            width={28}
+                            height={28}
+                          />
+                          <p>{customer.name}</p>
+                        </div>
+                      </div>
+                      <p className="text-sm text-gray-500">
+                        {customer.email}
+                      </p>
+                    </div>
+                  </div>
+                  <div className="flex w-full items-center justify-between border-b py-5">
+                    <div className="flex w-1/2 flex-col">
+                      <p className="text-xs">Pending</p>
+                      <p className="font-medium">{customer.total_pending}</p>
+                    </div>
+                    <div className="flex w-1/2 flex-col">
+                      <p className="text-xs">Paid</p>
+                      <p className="font-medium">{customer.total_paid}</p>
+                    </div>
+                  </div>
+                  <div className="pt-4 text-sm">
+                    <p>{customer.total_invoices} invoices</p>
+                  </div>
+                </div>
+              ))}
+            </div>
+
+            {/* =================================== */}
+            {/* == BLOK KODE UNTUK DESKTOP VIEW == */}
+            {/* =================================== */}
             <table className="hidden min-w-full rounded-md text-gray-900 md:table">
               <thead className="rounded-md bg-gray-50 text-left text-sm font-normal">
                 <tr>
@@ -23,7 +70,7 @@ export default async function CustomersTable({ query }: { query: string }) {
                 </tr>
               </thead>
               <tbody className="divide-y divide-gray-200 text-gray-900">
-                {customers.map((customer: { id: Key | null | undefined; image_url: string | StaticImport; name: string | number | bigint | boolean | ReactElement<unknown, string | JSXElementConstructor<any>> | Iterable<ReactNode> | ReactPortal | Promise<string | number | bigint | boolean | ReactPortal | ReactElement<unknown, string | JSXElementConstructor<any>> | Iterable<ReactNode> | null | undefined> | null | undefined; email: string | number | bigint | boolean | ReactElement<unknown, string | JSXElementConstructor<any>> | Iterable<ReactNode> | ReactPortal | Promise<string | number | bigint | boolean | ReactPortal | ReactElement<unknown, string | JSXElementConstructor<any>> | Iterable<ReactNode> | null | undefined> | null | undefined; total_invoices: string | number | bigint | boolean | ReactElement<unknown, string | JSXElementConstructor<any>> | Iterable<ReactNode> | ReactPortal | Promise<string | number | bigint | boolean | ReactPortal | ReactElement<unknown, string | JSXElementConstructor<any>> | Iterable<ReactNode> | null | undefined> | null | undefined; total_pending: string | number | bigint | boolean | ReactElement<unknown, string | JSXElementConstructor<any>> | Iterable<ReactNode> | ReactPortal | Promise<string | number | bigint | boolean | ReactPortal | ReactElement<unknown, string | JSXElementConstructor<any>> | Iterable<ReactNode> | null | undefined> | null | undefined; total_paid: string | number | bigint | boolean | ReactElement<unknown, string | JSXElementConstructor<any>> | Iterable<ReactNode> | ReactPortal | Promise<string | number | bigint | boolean | ReactPortal | ReactElement<unknown, string | JSXElementConstructor<any>> | Iterable<ReactNode> | null | undefined> | null | undefined; }) => (
+                {customers.map((customer: CustomersTableType) => (
                   <tr key={customer.id} className="group">
                     <td className="whitespace-nowrap bg-white py-5 pl-4 pr-3 text-sm text-black group-first-of-type:rounded-md group-last-of-type:rounded-md sm:pl-6">
                       <div className="flex items-center gap-3">
